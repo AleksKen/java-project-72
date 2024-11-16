f418028fac2bf2cacd1a670048429f3d61acf1ec
AleksKen <aleks.ken2017@mail.ru>
2024-11-16 21:59:09 +0300

    Анализ страницы (подрубаю postgresql)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,8 @@ dependencies {
     //БД
     implementation("com.h2database:h2:2.2.224")
     implementation("com.zaxxer:HikariCP:5.1.0")
+    implementation("org.postgresql:postgresql:42.7.3")
+
 }
 
 application {
